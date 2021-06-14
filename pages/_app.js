c933fc631b808f0925362ae171fccf4289bbe025
Alasdair Blackwell <aliblackwell@protonmail.com>
2021-06-14 11:04:20 +0100

    Add Material UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,23 @@
 import "../styles/globals.css"
 import Header from "../layout-components/header"
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+//import { ThemeProvider } from "@material-ui/core/styles"
+//import theme from "../styles/theme"
+
 import { SkipNavLink, SkipNavContent } from "@reach/skip-nav"
 import "@reach/skip-nav/styles.css"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <SkipNavLink />
-      <Header></Header>
-      <SkipNavContent />
-      <Component {...pageProps} />
+     
+        <CssBaseline />
+        <SkipNavLink />
+        <Header></Header>
+        <SkipNavContent />
+        <Component {...pageProps} />
+    
     </>
   )
 }
