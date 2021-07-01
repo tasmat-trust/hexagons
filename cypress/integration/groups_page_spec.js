42b5e172fc48647fca91227446d6136a07482e01
Ali Blackwell <aliblackwell@protonmail.com>
2021-07-01 14:31:44 +0100

    Test create groups

--- cypress/integration/groups_page_spec.js
+++ cypress/integration/groups_page_spec.js
@@ -2,10 +2,11 @@ describe('Manage Groups Page', () => {
 
   beforeEach(() => { 
     cy.login(('Teacher'))
+    cy.visit('/groups')
   })
 
-  it('shows groups to logged in user', () => {
-    cy.visit('/groups')
+  it('shows groups to logged in user', () => {    
     cy.get('[data-test-id=title]').should('be.visible')
   })
+
 })
--- cypress/integration/manage_pupils_page_spec.js
+++ cypress/integration/manage_pupils_page_spec.js
@@ -3,14 +3,24 @@ describe('Manage Pupils Page', () => {
   beforeEach(() => {
 
     cy.login(('Senior Leader'))
-
+    cy.visit('/manage/pupils')
   })
 
   it('shows manage pupil interface to Senior Leaders', () => {
-    cy.visit('/manage/pupils')
+    
     cy.get('[data-test-id=title]').should('be.visible')
     cy.get('[data-test-id=notification]').should('be.visible')
     cy.get('[data-test-id=new-pupil]').should('be.visible')
     cy.get('[data-test-id=new-group]').should('be.visible')
+  })
+
+  it('Lets SLT create new groups', () => {
+
+    cy.get('.MuiGrid-grid-md-5 > .makeStyles-paper-37 > .MuiBox-root > .MuiButtonBase-root > .MuiButton-label').click();
+    cy.get('#name').clear();
+    cy.get('#name').type('New group');
+    cy.get('#new-group > :nth-child(2) > .MuiButtonBase-root > .MuiButton-label').click();
+    cy.get('.MuiDialogActions-root > .MuiButtonBase-root > .MuiButton-label').click();
+    cy.get('h3').contains('New group')
   })
 })
