73063214c43d574bb3c9c37af968fc1f2f0bbe1c
Ali Blackwell <aliblackwell@protonmail.com>
2021-07-01 10:21:15 +0100

    Testing coverage stats

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    eslint: {
-        dirs: ['components', 'pages', 'queries', 'styles', 'cypress'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
-    },
-    images: {
-        domains: ['localhost']
-    }
+  eslint: {
+    dirs: ['components', 'pages', 'queries', 'styles', 'cypress'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
+  images: {
+    domains: ['localhost']
+  }
 }
