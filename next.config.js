7ec3b9fabfcfe0d6a477dd9c253f3e8d1f0417ec
Alasdair Blackwell <aliblackwell@protonmail.com>
2021-08-26 18:35:00 +0100

    Add new backends

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ module.exports = {
     dirs: ['components', 'pages', 'queries', 'styles', 'cypress'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
   },
   images: {
-    domains: ['localhost', 'strapi-bfxo.onrender.com', 'backend.hexagons.app', 'hexagons-backend-znrzh.ondigitalocean.app']
-  }
-}
+    domains: [
+      'localhost',
+      'strapi-bfxo.onrender.com',
+      'backend.hexagons.app',
+      'hexagons-backend-znrzh.ondigitalocean.app',
+      'stm-backend.hexagons.app',
+      'tasmat-backend.hexagons.app',
+    ],
+  },
+};
