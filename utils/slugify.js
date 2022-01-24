92f30c5469727e757a1eb009449fdfbecf42615a
Alasdair Blackwell <aliblackwell@protonmail.com>
2022-01-24 10:24:00 +0000

    Remove unneeded slugify

--- utils/slugify.js
+++
@@ -1,7 +0,0 @@
-export default function slugify(stringToSlugify) {
-  let slug = stringToSlugify.replace(/,/g, '-');
-  slug = str.replace(/\s/g, '-');
-  slug = slug.replace(/\s+/g, '-');
-  slug = slug.replace(/&/g, "and")
-  return slug;
-}
