dc9507b59ade331496707cd6feec8153f44945b4
Alasdair Blackwell <aliblackwell@protonmail.com>
2023-04-16 10:34:27 +0100

    Ensure sort levels by step/stage, resolve Step 6 bug

--- utils/sortLevelsAndModules.js
+++ utils/sortLevelsAndModules.js
@@ -1,5 +1,5 @@
-function sortBy(modules, level) {
-  return  modules.sort((a, b) => {
+function sortModuleBy(items, level) {
+  return  items.sort((a, b) => {
     const aLevel = a.level === level ? 'a' : 'b'
     const bLevel = b.level === level ? 'a' : 'b'
     return aLevel.localeCompare(bLevel)
@@ -7,21 +7,31 @@ function sortBy(modules, level) {
 }
 
 function sortModules(modules) {
-  modules.sort((a, b) => a.order - b.order)
-  sortBy(modules, 'phase')
-  sortBy(modules, 'band')
-  sortBy(modules, 'stage')
-  sortBy(modules, 'step')
-  return modules
+  let sortedModules = modules
+  sortedModules = sortedModules.sort((a, b) => a.order - b.order)
+  sortedModules = sortModuleBy(sortedModules, 'phase')
+  sortedModules = sortModuleBy(sortedModules, 'band')
+  sortedModules = sortModuleBy(sortedModules, 'stage')
+  sortedModules = sortModuleBy(sortedModules, 'step')
+  return sortedModules
+}
+
+function sortLevelBy(items, level) {
+  return  items.sort((a, b) => {
+    const aLevel = a.module.level === level ? 'a' : 'b'
+    const bLevel = b.module.level === level ? 'a' : 'b'
+    return aLevel.localeCompare(bLevel)
+  })
 }
 
 function sortLevels(levels) {
-  levels.sort((a, b) => a.module.order - b.module.order)
-  sortBy(levels, 'phase')
-  sortBy(levels, 'band')
-  sortBy(levels, 'stage')
-  sortBy(levels, 'step')
-  return levels
+  let sortedLevels = levels
+  sortedLevels = sortedLevels.sort((a, b) => a.module.order - b.module.order)
+  sortedLevels = sortLevelBy(sortedLevels, 'phase')
+  sortedLevels = sortLevelBy(sortedLevels, 'band')
+  sortedLevels = sortLevelBy(sortedLevels, 'stage')
+  sortedLevels = sortLevelBy(sortedLevels, 'step')
+  return sortedLevels
 }
 
 export {
