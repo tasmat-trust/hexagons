083cfdb5f9ae6df1574c76fbaab7d581559d4c73
Alasdair Blackwell <aliblackwell@protonmail.com>
2023-04-24 19:55:34 +0100

    Filter for functional subjects

--- utils/getNameFromCamelCased.js
+++ utils/getNameFromCamelCased.js
@@ -1,2 +1,6 @@
-const getNameFromCamelCased = (str) => str.split(/([A-Z][a-z]+)/).filter(Boolean).join(' ')
+const nameFromCamelCased = (str) => str.split(/([A-Z][a-z]+)/).filter(Boolean).join(' ')
+const getNameFromCamelCased = (str) => {
+    const n = nameFromCamelCased(str)
+    return n.includes('Functional') ? n.replace('Functional', 'PfA') : n
+}
 export default getNameFromCamelCased
